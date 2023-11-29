5759357d663ca50386751e0b15d1b65510b4877d
kris <ywkwhu@163.com>
2023-11-29 19:24:50 +0800

    剪枝提速

--- leetcode/editor/com/39.combination-sum.java
+++ leetcode/editor/com/39.combination-sum.java
@@ -61,12 +61,23 @@
  * 1 <= target <= 40
  * 
  * 
+ *  * @param candidates 候选数组
+     * @param begin      搜索起点
+     * @param len        冗余变量，是 candidates 里的属性，可以不传
+     * @param target     每减去一个元素，目标值变小
+     * @param cur       从根结点到叶子结点的路径，是一个栈
+     * @param res        结果集列表
+
+作者：liweiwei1419
+链接：https://leetcode.cn/problems/combination-sum/solutions/14697/hui-su-suan-fa-jian-zhi-python-dai-ma-java-dai-m-2/
  * 
  * solution1 
  * 深度优先搜索算法
  * 
  * 
- * 
+ * 剪枝提速
+根据上面画树形图的经验，如果 target 减去一个数得到负数，那么减去一个更大的树依然是负数，同样搜索不到结果。基于这个想法，我们可以对输入数组进行排序，添加相关逻辑达到进一步剪枝的目的；
+排序是为了提高搜索速度，对于解决这个问题来说非必要。但是搜索问题一般复杂度较高，能剪枝就尽量剪枝。实际工作中如果遇到两种方案拿捏不准的情况，都试一下。
  * solution2 
  * 1 先排序，剪枝
  * 2 再使用深度优先搜索算法
@@ -100,9 +111,12 @@ class Solution1 {
                combinations.add(new ArrayList(cur)); 
                return;
             }
+            // 重点理解这里从 begin 开始搜索的语意
             for (int i = begin; i < length; i++){
                 cur.addLast(candidates[i]);
+                 // 注意：由于每一个元素可以重复使用，下一轮搜索的起点依然是 i，这里非常容易弄错
                 dfs(candidates,target-candidates[i], i, length,combinations,cur);
+                // 状态重置
                 cur.removeLast();
             }
 
@@ -115,6 +129,7 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> combinations = new ArrayList<>();
         int length = candidates.length;
+        // 排序是剪枝的前提
         Arrays.sort(candidates); 
         Deque<Integer> cur = new ArrayDeque<>();
         if(length==0){
@@ -126,11 +141,13 @@ class Solution {
 
 
         public void dfs(int[] candidates, int target, int begin, int length, List<List<Integer>> combinations, Deque<Integer> cur ){
+            // 由于进入更深层的时候，小于 0 的部分被剪枝，因此递归终止条件值只判断等于 0 的情况
             if(target == 0){
                combinations.add(new ArrayList(cur)); 
                return;
             }
             for (int i = begin; i < length; i++){
+                
                 if(target < candidates[i]) {
                     break;
                 }
