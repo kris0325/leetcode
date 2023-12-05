d7dd913dd016dacded723170fdeefa5f06bde3e7
kris <ywkwhu@163.com>
2023-12-05 16:47:55 +0800

    migrate

---
+++ 90.subsets-ii.java
@@ -0,0 +1,47 @@
+/*
+ * @lc app=leetcode id=90 lang=java
+ *
+ * [90] Subsets II
+ *
+ * https://leetcode.com/problems/subsets-ii/description/
+ *
+ * algorithms
+ * Medium (56.68%)
+ * Likes:    9237
+ * Dislikes: 265
+ * Total Accepted:    819.4K
+ * Total Submissions: 1.4M
+ * Testcase Example:  '[1,2,2]'
+ *
+ * Given an integer array nums that may contain duplicates, return all possible
+ * subsets (the power set).
+ * 
+ * The solution set must not contain duplicate subsets. Return the solution in
+ * any order.
+ * 
+ * 
+ * Example 1:
+ * Input: nums = [1,2,2]
+ * Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+ * Example 2:
+ * Input: nums = [0]
+ * Output: [[],[0]]
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 1 <= nums.length <= 10
+ * -10 <= nums[i] <= 10
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        
+    }
+}
+// @lc code=end
+
