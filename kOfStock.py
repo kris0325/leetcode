8e96a8027bcc8918e7c0cd5eae0ebfc78e1a90c3
kris <ywkwhu@163.com>
2023-12-29 15:31:36 +0800

    股票价格显示蜡烛图

---
+++ kOfStock.py
@@ -0,0 +1,16 @@
+import plotly.graph_objects as go
+import pandas as pd
+
+# 从数据源（例如CSV文件）中读取股票价格数据，这里以苹果公司（AAPL）的股票数据为例
+# 请确保已经安装了pandas库，并且已经有了股票价格数据文件
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
+
+# 创建蜡烛图
+fig = go.Figure(data=[go.Candlestick(x=df['Date'],
+                open=df['AAPL.Open'],
+                high=df['AAPL.High'],
+                low=df['AAPL.Low'],
+                close=df['AAPL.Close'])])
+
+# 显示蜡烛图
+fig.show()
