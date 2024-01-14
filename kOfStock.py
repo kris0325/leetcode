c91eff8c2fe386199a3b018f2ed25541ec322c8b
kris <ywkwhu@gmail.com>
2024-01-14 22:10:48 +0800

    python

--- kOfStock.py
+++ kOfStock.py
@@ -1,6 +1,7 @@
 import plotly.graph_objects as go
 import pandas as pd
 
+# python 
 # 从数据源（例如CSV文件）中读取股票价格数据，这里以苹果公司（AAPL）的股票数据为例
 # 请确保已经安装了pandas库，并且已经有了股票价格数据文件
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/finance-charts-apple.csv')
